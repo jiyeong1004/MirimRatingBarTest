72d5d957911a0ca9f5e020cc287af7429faa088e
jiyeong1004 <ak1157@naver.com>
2020-11-23 19:27:48 +0900

    ok

--- app/src/main/java/com/example/mirimratingbartest/MainActivity.java
+++ app/src/main/java/com/example/mirimratingbartest/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.mirimratingbartest;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    RatingBar ratingBar1, ratingBar2, ratingBar3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ratingBar1 = findViewById(R.id.rating_bar1);
+        ratingBar2 = findViewById(R.id.rating_bar2);
+        ratingBar3 = findViewById(R.id.rating_bar3);
+        Button btnIncr = findViewById(R.id.btn_incr);
+        Button btnDecr = findViewById(R.id.btn_decr);
+        btnIncr.setOnClickListener(btnListener);
+        btnDecr.setOnClickListener(btnListener);
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btn_incr:
+                    ratingBar1.setRating(ratingBar1.getRating() + ratingBar1.getStepSize());
+                    ratingBar2.setRating(ratingBar2.getRating() + ratingBar2.getStepSize());
+                    ratingBar3.setRating(ratingBar3.getRating() + ratingBar3.getStepSize());
+                    break;
+                case R.id.btn_decr:
+                    ratingBar1.setRating(ratingBar1.getRating() - ratingBar1.getStepSize());
+                    ratingBar2.setRating(ratingBar2.getRating() - ratingBar2.getStepSize());
+                    ratingBar3.setRating(ratingBar3.getRating() - ratingBar3.getStepSize());
+                    break;
+            }
+        }
+    };
 }
